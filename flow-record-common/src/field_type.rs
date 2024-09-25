a005125ceaa98365ee8b57e727d17ff626bf30c9
Jan Starke <jan.starke@posteo.de>
2024-09-25 12:30:22 +0200

    simplify tokenization

--- flow-record-common/src/field_type.rs
+++ flow-record-common/src/field_type.rs
@@ -25,29 +25,6 @@ pub enum FieldType {
     Path,
     UnixFileMode
 }
-/*
-impl Display for FieldType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                FieldType::Bool => "boolean",
-                FieldType::UInt16 => "uint16",
-                FieldType::UInt32 => "uint32",
-                FieldType::VarInt => "varint",
-                FieldType::Float => "float",
-                FieldType::String => "string",
-                FieldType::Bin => "bin",
-                FieldType::Datetime => "datetime",
-                FieldType::Filesize => "filesize",
-                FieldType::Path => "path",
-                FieldType::UnixFileMode => "unix_file_mode",
-            }
-        )
-    }
-}
-     */
 
 impl From<FieldType> for Value {
     fn from(value: FieldType) -> Self {
@@ -57,19 +34,6 @@ impl From<FieldType> for Value {
 
 impl ToTokens for FieldType {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let token = match self {
-            FieldType::Bool => quote! {FieldType::Bool},
-            FieldType::UInt16 => quote! {FieldType::UInt16},
-            FieldType::UInt32 => quote! {FieldType::UInt32},
-            FieldType::VarInt => quote! {FieldType::VarInt},
-            FieldType::Float => quote! {FieldType::Float},
-            FieldType::String => quote! {FieldType::String},
-            FieldType::Bin => quote! {FieldType::Bin},
-            FieldType::Datetime => quote! {FieldType::Datetime},
-            FieldType::Filesize => quote! {FieldType::Filesize},
-            FieldType::Path => quote! {FieldType::Path},
-            FieldType::UnixFileMode => quote! {FieldType::UnixFileMode},
-        };
-        tokens.append_all(quote! {flow_record_common::#token});
+        tokens.append_all(quote! {flow_record_common::#self});
     }
 }
